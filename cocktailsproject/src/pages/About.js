cffecfa183cc4c573241159f43cf81eeb4c0725b
StefanDokov <stefandokov2211@gmail.com>
2023-05-07 17:56:24 +0300

    cocktail project
    adding cocktail project

---
+++ cocktailsproject/src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+      Don’t Nod, makers of Life is Strange and Tell Me Why, 
+      has announced that its next title, Harmony: 
+      The Fall of Reverie, is coming this June and will get a playable demo 
+      this month. 
+      Steam and Switch players can pick up the game on June 8, 
+      whereas PlayStation 5 and Xbox Series X/S owners have to wait until 
+      June 22. Harmony: The Fall of Reverie is a choice-driven 
+      adventure starring Polly, a woman who returns home to find her missing 
+      mother. It's here Polly discovers she has clairvoyant abilities 
+      connecting her to a strange dimension called Reverie. 
+      While visiting this world, Polly becomes Harmony and encounters six 
+      powerful god-like beings called Aspirations, which serve as the hearts 
+      of humanity such as Glory, Power, Chaos, Bliss, and Truth. 
+      </p>
+    </section>
+  )
+}
+
+export default About

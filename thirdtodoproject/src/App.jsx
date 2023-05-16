b4c8e7a77b3c694559a31aaf611ef545ee1b3aab
StefanDokov <stefandokov2211@gmail.com>
2023-05-16 09:13:33 +0300

    adding local storage
    adding function to be able to use local storage with out application

--- thirdtodoproject/src/App.jsx
+++ thirdtodoproject/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import CustomForm from './components/CustomForm'
+import { useState } from 'react';
+import CustomForm from './components/CustomForm';
 import TaskList from './components/TaskList';
 import EditForm from './components/EditForm';
+import useLocalStorage from './hooks/useLocalStorage';
 
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useLocalStorage('react-todo-tasks', []);
   const [editedTask, setEditedTask] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
   const [previousFocusEl, setPreviousFocusEl] = useState(null);
